be1f147b527bffb864220646d81487aa6ce42364
jarrisonfor <yegoma@outlook.com>
2022-09-30 20:15:16 +0100

    Fix error de regla circular a la hora de borrarla, no se borraba el circulo exterior

--- js/tools/RuleCircle.js
+++ js/tools/RuleCircle.js
@@ -6,12 +6,12 @@ class RuleCircle extends Rule {
     }
 
     startAddingLine(event) {
-        super.startAddingLine(event)
+        super.startAddingLine(event);
         this.createCircle();
     }
 
     startDrawingLine(event) {
-        super.startDrawingLine(event)
+        super.startDrawingLine(event);
         if (this.line) {
             this.adjustCircleRadiusAndPosition();
         }
@@ -34,12 +34,13 @@ class RuleCircle extends Rule {
         });
         this.canvas.add(circle);
         this.line.circle = circle;
+        this.line.associatedChild = circle;
         this.canvas.requestRenderAll();
     }
 
     pointersFollowLine() {
         super.pointersFollowLine();
-        this.adjustCircleRadiusAndPosition()
+        this.adjustCircleRadiusAndPosition();
     }
 
     lineFollowPointers() {
@@ -50,11 +51,19 @@ class RuleCircle extends Rule {
     adjustCircleRadiusAndPosition() {
         let newLineCoords = this.getNewLineCoordinates();
         this.line.circle?.set({
-            radius: Math.sqrt(Math.pow(newLineCoords.x2*1-newLineCoords.x1*1, 2)+Math.pow(newLineCoords.y2*1-newLineCoords.y1*1, 2)),
+            radius: Math.sqrt(Math.pow(newLineCoords.x2 * 1 - newLineCoords.x1 * 1, 2) + Math.pow(newLineCoords.y2 * 1 - newLineCoords.y1 * 1, 2)),
             top: newLineCoords.y1,
             left: newLineCoords.x1,
         })
         this.canvas.requestRenderAll();
+    }
+
+    addingControlPoints() {
+        super.addingControlPoints();
+        if (this.line.pointer1) {
+            this.line.circle.associatedChild = this.line.pointer1;
+            this.line.associatedChild = this.line.circle
+        }
     }
 
 }
