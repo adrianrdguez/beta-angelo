7ad57ce4a36137eb9dcd650451778f438f87e804
adrianrdguez <adrian.rodriguez@singularfactory.com>
2022-10-04 16:06:32 +0100

    add measurement to single rule

--- js/tools/Rule.js
+++ js/tools/Rule.js
@@ -13,7 +13,13 @@ class Rule extends Tool {
         this.element.line = new fabric.Line([pointer.x, pointer.y, pointer.x, pointer.y], {
             stroke: this.canvas.freeDrawingBrush.color,
             strokeWidth: this.canvas.freeDrawingBrush.width,
+            startx: new Array(),
+            starty: new Array(),
+            endx: new Array(),
+            endy: new Array(),
+            temp: 0
         });
+
         this.setDefaultObjectOptions(this.element.line);
         this.element.line.set({
             hasBorders: false,
@@ -21,6 +27,8 @@ class Rule extends Tool {
         this.element.line.setControlsVisibility({
             mtr: false,
         })
+        this.element.line.startx[this.element.line.temp] = pointer.x;
+        this.element.line.starty[this.element.line.temp] = pointer.y;
         this.canvas.add(this.element.line);
         this.canvas.requestRenderAll();
     }
@@ -32,12 +40,27 @@ class Rule extends Tool {
                 x2: pointer.x,
                 y2: pointer.y
             });
+            this.element.line.endx[this.element.line.temp] = pointer.x;
+            this.element.line.endy[this.element.line.temp] = pointer.y;
             this.canvas.requestRenderAll()
         }
     }
 
+    Calculate = (x1, y1, x2, y2) => {
+        return Math.sqrt(Math.pow(x2 * 1 - x1 * 1, 2) + Math.pow(y2 * 1 - y1 * 1, 2));
+    }
+
     stopDrawingLine() {
         this.element.line.setCoords();
+        var px = this.Calculate(this.element.line.startx[this.element.line.temp], this.element.line.starty[this.element.line.temp], this.element.line.endx[this.element.line.temp], this.element.line.endy[this.element.line.temp]).toFixed(2);
+        this.element.text = new fabric.Text(px, {
+            left: this.element.line.startx[this.element.line.temp] + ((this.element.line.endx[this.element.line.temp] - this.element.line.startx[this.element.line.temp]) / 2),
+            top: this.element.line.starty[this.element.line.temp] + ((this.element.line.endy[this.element.line.temp] - this.element.line.starty[this.element.line.temp]) / 2),
+            fontSize: 12,
+            stroke: 'red'
+        });
+        //console.log('angle', Math.atan((this.canvas.line.endy[this.canvas.line.temp] - this.canvas.line.starty[this.canvas.line.temp]) / (this.canvas.line.endx[this.canvas.line.temp] - this.canvas.line.startx[this.canvas.line.temp])) * 180 / Math.PI)
+        this.canvas.add(this.element.text);
         this.element.line.on('mousedblclick', () => this.addingControlPoints());
         this.element.line.on('moving', () => this.pointersFollowLine());
         this.canvas.simulator.setCurrentTool(new Drag(this.canvas));
