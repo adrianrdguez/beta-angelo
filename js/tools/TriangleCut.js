752a0f0497b6b5f0a227fd484b7f8bf0e2f26ed2
jarrisonfor <yegoma@outlook.com>
2022-10-06 13:36:24 +0100

    Creamos puntero para el corte

--- js/tools/TriangleCut.js
+++ js/tools/TriangleCut.js
@@ -10,4 +10,40 @@ class TriangleCut extends RuleTriangle {
         });
     }
 
+    addingControlPoints() {
+        if (!this.element.pointer4) {
+            super.addingControlPoints();
+            this.element.pointer1?.set({
+                stroke: 'blue',
+                fill: 'blue',
+            });
+            this.element.pointer1?.on('mousedblclick', () => this.createCutterPointer())
+        }
+    }
+
+    createCutterPointer() {
+        let pointerCoords = this.getNewLineCoordinates();
+        if (!this.element.pointer4) {
+            
+            this.element.line4 = this.createLine(this.element.line3.x1, this.element.line3.y1, this.element.line3.x2, this.element.line3.y2);
+            this.element.pointer4 = this.createPointer(pointerCoords.pointer1Coords.top, pointerCoords.pointer1Coords.left);
+            this.element.pointer4.set({
+                radius: this.element.pointer4.radius + 10,
+                stroke: 'lightblue',
+                fill: 'lightblue',
+            });
+            this.element.pointer4.off('moving');
+            this.element.pointer4.on('moving', () => this.line4FollowPointer4())
+            this.canvas.discardActiveObject();
+            this.canvas.requestRenderAll();
+        }
+    }
+
+    line4FollowPointer4() {
+        this.element.line4.set({
+            x1: this.element.pointer4.left,
+            y1: this.element.pointer4.top,
+        })
+        this.element.line4.setCoords();
+    }
 }
