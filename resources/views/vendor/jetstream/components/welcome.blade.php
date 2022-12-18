8bfa3ca869bf5fefbe317b646e60d307b427d71f
jarrisonfor <yegoma@outlook.com>
2022-12-18 13:37:59 +0000

    Añadimos las librerias necesarias para usar crear componentes en front

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="projects">
-    <a href="{{ route('simulator') }}" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Crea tu nuevo proyecto.</h5>
-    </a>
-</div>
-
--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/css/bootstrap.min.css',
                 'resources/css/app.css',
+                'node_modules/tw-elements/dist/js/index.min.js'
             ],
             refresh: [
                 ...refreshPaths,
