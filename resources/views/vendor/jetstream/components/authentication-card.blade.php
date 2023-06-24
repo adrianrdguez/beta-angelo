81b25c6d33ad950e0eb11451aa2438d4e88f7db7
jarrisonfor <yegoma@outlook.com>
2023-06-24 16:08:03 +0100

    Cambiamos footer del gobierno para que sea mas responsive

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -7,27 +7,4 @@
         {{ $slot }}
     </div>
 
-    <div class="flex-grow"></div>
-
-    <div class="w-full bg-white py-6 px-6 sm:rounded-lg mt-8 mx-6">
-        <div class="flex justify-center items-center">
-            <div class="flex flex-col items-center">
-                <img src="{{ asset('img/gobierno2.jpg') }}" alt="Image 1" class="w-44 h-20 mb-2">
-            </div>
-            <div class="flex flex-col items-center" style="padding-left: 50px; padding-right: 50px;">
-                <img src="{{ asset('img/gobierno1.png') }}" alt="Image 2" class="w-60 h-16 mb-2">
-            </div>
-            <div class="flex flex-col items-center">
-                <img src="{{ asset('img/gobierno3.jpeg') }}" alt="Image 3" class="w-60 h-16 mb-2">
-            </div>
-        </div>
-
-        <div class="text-center mt-4" style="padding-left: 200px; padding-right: 200px;">
-            <p class="text-gray-600 md:text-sm">Proyecto “Desarrollo de nuevos implantes ortopédicos
-                veterinarios para el
-                tratamiento quirúrgico de patologías de la cadera y la rodilla” cofinanciado por el programa de
-                subvenciones a empresas de alta tecnología e intensivas en conocimiento en áreas prioritarias de la RIS3
-                (EATIC 2022), cofinanciado por el Fondo Europeo de Desarrollo Regional (FEDER)</p>
-        </div>
-    </div>
 </div>
